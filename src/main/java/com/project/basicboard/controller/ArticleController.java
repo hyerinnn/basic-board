0c5bbccc5d7abf6d813042d9bbbaf907253f2cc1
hyerinnn <eun8229@gmail.com>
2023-09-07 01:54:42 +0900

    #16 - detail 페이지 추가
    - detail 페이지 생성, 컨트롤러/테스트까지 테스트 완료
    - 푸터, 헤더에 id값 추가

--- src/main/java/com/project/basicboard/controller/ArticleController.java
+++ src/main/java/com/project/basicboard/controller/ArticleController.java
@@ -3,9 +3,11 @@ package com.project.basicboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Locale;
 
 @RequestMapping("/articles")
 @Controller
@@ -17,5 +19,16 @@ public class ArticleController {
         map.addAttribute("articles", List.of());
 
         return "articles/index";
+    }
+
+
+    @GetMapping("/{aricleId}")
+    public String article(@PathVariable Long aricleId,ModelMap map){
+
+        map.addAttribute("article","article");
+        map.addAttribute("articleComments", List.of());
+
+
+        return "articles/detail";
     }
 }
