a72123dedcd4d966e7c0cdfa209732d560d8569d
hyerinnn <eun8229@gmail.com>
2023-09-11 23:15:52 +0900

    #20 - 임시커밋

---
+++ src/main/java/com/project/basicboard/service/PaginationService.java
@@ -0,0 +1,19 @@
+package com.project.basicboard.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaginationService {
+
+    private static final int BAR_LENGTH = 5;
+
+    public List<Integer> getPaginationBarNumbers(int currentPageNumber,int totalPages){
+        return null;
+    }
+
+    public int currentBarLength(){
+        return BAR_LENGTH;
+    }
+}
