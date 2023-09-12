084a516766db619a2a9bd9cd4f7cf5bef01d5fd0
hyerinnn <eun8229@gmail.com>
2023-09-12 17:05:23 +0900

    # 도메인 업데이트 : UserAccount 회원 ID에 유니크 키 추가

--- src/test/java/com/project/basicboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/project/basicboard/repository/JpaRepositoryTest.java
@@ -56,7 +56,7 @@ class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFine(){
         //given
         long previousCount = articleRepository.count();
-        UserAccount userAccount = userAccountRepository.save(UserAccount.of("uno", "pw", null, null, null));
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("hyerin", "pw", null, null, null));
         Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
         //when
